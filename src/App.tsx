d49168ea45ca915b60c07a48d4d88b4a7a1b695e
Sammynator <sgerules@gmail.com>
2025-10-22 13:33:06 +0200

    shared components imported

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import './i18n';
 import { Provider } from 'react-redux';
 import store from '@stores/index.ts';
 import Newsfeed from '@pages/Newsfeed/Newsfeed.tsx';
+import {
+  createCustomJoyTheme,
+  createCustomMuiTheme,
+} from '@agile-software/shared-components';
 
 const theme = createCustomTheme({
   colorSchemes: {
