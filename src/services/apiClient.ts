3ca7266d3040654665aca4b0cdcc06acb648e1ce
Jakob Fischer <jako.fischer@outlook.de>
2025-10-28 10:48:28 +0100

    fix: update BASE_URL to use localhost for local development

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,5 +1,5 @@
 // src/services/apiClient.ts
-const BASE_URL = 'http://[::1]:8080';
+const BASE_URL = 'http://localhost:8080';
 //const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://sau-portal.de/api/newsfeed';
 
 
